845c5d312cb7e43d272dc54dc5c68237b7fe3074
saimon322 <saimon.kat23@gmail.com>
2020-12-22 17:28:44 +0300

    footer acf updates

--- footer.php
+++ footer.php
@@ -8,27 +8,30 @@
         <div class="main-footer__top">
             <div class="main-footer__main">
                 <div class="main-footer__info">
-                    <p>Haven Hills has been lifting up domestic violence survivors since 1977. Alongside our community allies, we provide safety, shelter, and support to all survivors while working to break the cycle of abuse.</p>
-                    <p>Call our 24/7 Crisis Line at 818 887 6589.</p>
-                    <p>Follow us at @havenhills and donate today at havenhills.org</p>
+                    <?php the_field('footer_main_text', 'option'); ?>
+                    
+                    <div class="main-footer__info-block">
+                        <?php the_field('footer_info_block_1', 'option'); ?>
+                    </div>
+
+                    <div class="main-footer__info-block">
+                        <?php the_field('footer_info_block_2', 'option'); ?>
+                    </div>
                 </div><!-- /.main-footer__info -->
 
                 <div class="main-footer__socials">
-                    <a href="https://www.facebook.com/havenhills" target="_blank" rel="noopener noreferrer">
-                        <svg width='23' height='23'>
-                            <use xlink:href='#icon-facebook'></use>
+                    <?php if( have_rows('socials', 'option') ): ?>
+                    <?php while( have_rows('socials', 'option') ): the_row();
+                    $name = get_sub_field('social_name');
+                    $link = get_sub_field('social_url');
+                    ?>
+                    <a href="<?php echo $link; ?>" target="_blank" rel="noopener noreferrer">
+                        <svg width='15' height='15'>
+                            <use xlink:href='#icon-<?php echo $name; ?>'></use>
                         </svg>
                     </a>
-                    <a href="https://twitter.com/havenhills" target="_blank" rel="noopener noreferrer">
-                        <svg width='23' height='23'>
-                            <use xlink:href='#icon-twitter'></use>
-                        </svg>
-                    </a>
-                    <a href="https://www.instagram.com/havenhills/" target="_blank" rel="noopener noreferrer">
-                        <svg width='23' height='23'>
-                            <use xlink:href='#icon-instagram'></use>
-                        </svg>
-                    </a>
+                    <?php endwhile; ?>
+                    <?php endif; ?>
                 </div><!-- /.main-footer__socials -->
             </div>
 
@@ -43,18 +46,20 @@
 
         <div class="main-footer__bottom">
             <div class="main-footer__copyright">
-                Copyright © 2020 Haven Hills. All Rights Reserved<br>
-                Web design by 
+                <?php the_field('footer_copyright', 'option'); ?>
             </div>
             <div class="main-footer__taxes">
-                Haven Hills is a 501(c)3 nonprofit organization<br>
-                and federal tax ID number is 95-3196247
+                <?php the_field('footer_taxes', 'option'); ?>
             </div>
         </div>
     </div>
 </footer><!-- / .main-footer -->
 
-<a href="/donate.html" class="button donate-button">Donate</a>
+<!-- Donate button -->
+<?php $donate = get_field('donate_button', 'option'); ?>
+<a href="<?php echo $donate['url']; ?>" class="button donate-button">
+    <?php echo $donate['title']; ?>
+</a>
 
 <?php wp_footer(); ?>
 
